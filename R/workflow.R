2bd3c0216c15a4dec26b5eed8ff94e135a48d4f0
Oliver Moravčík <o.moravcik@gmail.com>
2018-04-29 13:14:38 +0200

    upsampling now logicaly right

--- R/workflow.R
+++ R/workflow.R
@@ -101,32 +101,36 @@ BirchSPADE.run.analysis <- function(input_file_full             # full path to t
   # methods <- c("ward", "average", "single", "complete")
   hclust.result <- Rclusterpp.hclust(subclusters, method = "ward", distance = "euclidean")
   subclusters$hier_cluster = cutree(hclust.result, k = final_cluster_count)
+  # get hier_clusters centroids
+  hier_cluster_centroids <- aggregate(subclusters, by=list(subclusters$hier_cluster),FUN=mean)[,-1] # first column is the group
   hclust_end_time <- Sys.time()
   message(paste0("Hierarchical clustering took time (seconds): ",
                round(difftime(hclust_end_time, hclust_start_time, units='secs'), digits = 2)))
 
 
   ## 4 # upsampling using K-means
-  # need to find for every point from cells_data closest sub_cluster from birch/ hier_cluster from hclust with k-means, 1 iteration
+  # need to find for every point from cells_data closest sub_cluster from birch with k-means, 1 iteration
   message("Upsampling fcs to clusters using k-means ... ")
   upsampling_start_time <- Sys.time()
-  # get hier_clusters centroids
-  hier_cluster_centroids <- aggregate(subclusters, by=list(subclusters$hier_cluster),FUN=mean)[,-1] # first column is the group
   # suppress warning that it did not konverge
   suppressWarnings(kmeans.result <- kmeans(x = cells_data,
-         centers = hier_cluster_centroids[,1:markers_cout], iter.max = kmeans_upsampling_iterations))
-  cells_data <- cbind(cells_data, "cluster" = kmeans.result$cluster)
+                                           centers = subclusters[,1:markers_cout],
+                                           iter.max = kmeans_upsampling_iterations,
+                                           algorithm = "Lloyd"))
+  #assign clusters to cells by subcluster assigment to hier clusters
+  cluster_col = subclusters$hier_cluster[kmeans.result$cluster]
+  cells_data = cbind(cells_data, "cluster" = cluster_col)
   # every cell has assigned final cluster
   upsampling_end_time <- Sys.time()
   message(paste0("Upsampling took time (seconds): ",
-               round(difftime(upsampling_end_time, upsampling_start_time, units='secs'), digits = 2)))
+                 round(difftime(upsampling_end_time, upsampling_start_time, units='secs'), digits = 2)))
 
 
   ## 5 # add asigned clusters to original not normalized data, bud without outliers if removal was used
   message("Saving orginal fcs data with assigned clusters so medians and others can be computed ... ")
   saving_start_time <- Sys.time()
   saving.returns = BirchSPADE.save_original_data_with_clusters(input_file_full, outputs_dir,
-                                                              kmeans.result$cluster, comp, transforms,
+                                                               cluster_col, comp, transforms,
                                                               remove_outliers, rows_without_outliers)
   saving_end_time <- Sys.time()
   message(paste0("Saving fcs with clusters took time (seconds): ",
